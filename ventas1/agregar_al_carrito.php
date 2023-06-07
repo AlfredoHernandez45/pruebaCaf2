bd146a455bb79b6ba39dc369df5d6709e59dd113
JoseConsultores98 <jose@consultoresweb.com.mx>
2023-06-06 23:25:28 -0500

    Actualizacion de informacion obtenida

--- ventas1/agregar_al_carrito.php
+++ ventas1/agregar_al_carrito.php
@@ -1,20 +1,25 @@
 <?php
 // incluir archivos de configuración de la base de datos
 require_once('conexion.php');
-// verificar si se recibió el ID del artículo
-if (isset($_POST['articuloID'])) {
-    $articuloID = $_POST['articuloID'];
 
-    // Aquí deberías realizar las validaciones necesarias y asegurarte de que el artículo exista y sea válido antes de realizar la inserción en la base de datos.
+// verificar si se recibió el formulario con la información del artículo
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Obtener la información del formulario
+    $nombre = $_POST['nombre'];
+    $cveArticulo = $_POST['cveArticulo'];
+    $precio = $_POST['precio'];
+    $cantidad = $_POST['cantidad'];
+
+    // Aquí deberías realizar las validaciones necesarias y asegurarte de que la información del artículo sea válida antes de realizar la inserción en la base de datos.
 
     // Ejemplo de inserción en la tabla del carrito
-    $sql = "INSERT INTO carrito (id_compra) VALUES ($articuloID)";
+    $sql = "INSERT INTO carrito (cveArticulo, cantidadProducto, totalPago) VALUES ('$cveArticulo', '$cantidad', '$precio')";
     if ($conn->query($sql) === TRUE) {
         echo 'Artículo agregado al carrito con éxito';
     } else {
         echo 'Error al agregar el artículo al carrito: ' . $conn->error;
     }
 } else {
-    echo 'No se recibió el ID del artículo';
+    echo 'No se recibió la información del artículo';
 }
 ?>
