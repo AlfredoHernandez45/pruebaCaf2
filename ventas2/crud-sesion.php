3d2084269bc57d2bc55737b6aae729d0eb21bf33
JoseConsultores98 <jose@consultoresweb.com.mx>
2023-06-11 16:17:05 -0500

    Agregado de sesion y registro de usuario

---
+++ ventas2/crud-sesion.php
@@ -0,0 +1,38 @@
+<?php
+// Configuración de la base de datos
+$host = 'localhost';
+$db = 'ventas_dos';
+$user = 'root';
+$password = '';
+
+// Conexión a la base de datos
+$conn = new mysqli($host, $user, $password, $db);
+
+// Verificar la conexión
+if ($conn->connect_error) {
+  die('Error de conexión: ' . $conn->connect_error);
+}
+
+// Procesar el formulario de inicio de sesión
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $email = $_POST['usuario'];
+  $password = $_POST['contrasena'];
+
+  // Consulta para verificar las credenciales del usuario
+  $sql = "SELECT * FROM usuarios WHERE correo = '$email' AND contrasena = '$password'";
+  $result = $conn->query($sql);
+
+  if ($result->num_rows > 0) {
+    // Usuario autenticado correctamente
+    //echo '¡Inicio de sesión exitoso!';
+    echo "<script> alert('Bienvenido de de nuevo'); window.location='mostrar.php' </script>";
+
+  } else {
+    // Credenciales inválidas
+    //echo 'Nombre de usuario o contraseña incorrectos.';
+    echo "<script> alert('Nombre de usuario o contraseña incorrectos... Intente de nuevo :)'); window.location='sesion.php' </script>";
+  }
+}
+// Cerrar la conexión
+$conn->close();
+?>
