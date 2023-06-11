3d789164f6ef32582db130f910911e1476fa0f12
JoseConsultores98 <jose@consultoresweb.com.mx>
2023-06-11 18:11:22 -0500

    Ajustes para mostrar los productos

--- ventas2/crud.php
+++ ventas2/crud.php
@@ -1,73 +1,73 @@
 <?php
+// incluye la clase Db
 require_once('conexion.php');
 require_once('articulo.php');
 
 class crudArticulo {
 	// constructor de la clase
-    public function __construct() {}
+	public function __construct() {}
 	// Funcion Crear
-    public function insertar($articulo) {
-        $db = Db::conectar();
-        $insert = $db->prepare('INSERT INTO Articulos (cveArticulo, nombre, precio) VALUES (:cveArticulo, :nombre, :precio)');
-        $insert->bindValue('cveArticulo', $articulo->getCveArticulo());
-        $insert->bindValue('nombre', $articulo->getNombre());
-        $insert->bindValue('precio', $articulo->getPrecio());
-        $insert->execute();
-    }
+	public function insertar($articulo) {
+		$db = Db::conectar();
+		$insert = $db->prepare('INSERT INTO Articulos (cveArticulo, nombre, precio) VALUES (:cveArticulo, :nombre, :precio)');
+		$insert->bindValue('cveArticulo', $articulo->getCveArticulo());
+		$insert->bindValue('nombre', $articulo->getNombre());
+		$insert->bindValue('precio', $articulo->getPrecio());
+		$insert->execute();
+	}
 
 	// Funcion Leer
-    public function mostrar() {
-        $db = Db::conectar();
-        $listaArticulos = [];
-        $select = $db->query('SELECT * FROM Articulos');
+	public function mostrar() {
+		$db = Db::conectar();
+		$listaArticulos = [];
+		$select = $db->query('SELECT * FROM Articulos');
 
-        foreach ($select->fetchAll() as $articulo) {
-            $myArticulo = new Articulo();
-            $myArticulo->setCveArticulo($articulo['cveArticulo']);
-            $myArticulo->setNombre($articulo['nombre']);
-            $myArticulo->setPrecio($articulo['precio']);
-            $listaArticulos[] = $myArticulo;
-        }
-
-        return $listaArticulos;
-    }
+		foreach($select->fetchAll() as $articulo){
+			$myArticulo = new Articulos();
+			$myArticulo->setCveArticulo($articulo['cveArticulo']);
+			$myArticulo->setNombre($articulo['nombre']);
+			$myArticulo->setPrecio($articulo['precio']);
+			$listaArticulos[]=$myArticulo;
+		}
+		return $listaArticulos;
+	}
 
 	// Funcion Eliminar datos
-    public function eliminar($cveArticulo) {
-        $db = Db::conectar();
-        $eliminar = $db->prepare('DELETE FROM Articulos WHERE cveArticulo = :cveArticulo');
-        $eliminar->bindValue('cveArticulo', $cveArticulo);
-        $eliminar->execute();
+	public function eliminar($cveArticulo) {
+		$db = Db::conectar();
+		$eliminar = $db->prepare('DELETE FROM Articulos WHERE cveArticulo = :cveArticulo');
+		$eliminar->bindValue('cveArticulo', $cveArticulo);
+		$eliminar->execute();
 
-        $eliminarExistencias = $db->prepare('DELETE FROM Existencias WHERE cveArticulo = :cveArticulo');
-        $eliminarExistencias->bindValue('cveArticulo', $cveArticulo);
-        $eliminarExistencias->execute();
-    }
+		//Delate de las funciones
+		$eliminar = $db->prepare('DELETE FROM Existencias WHERE cveArticulo = :cveArticulo');
+		$eliminar->bindValue('cveArticulo', $cveArticulo);
+		$eliminar->execute();
+	}
 
 	// Funcion Busqueda Espesifica
-    public function obtenerArticulo($cveArticulo) {
-        $db = Db::conectar();
-        $select = $db->prepare('SELECT * FROM Articulos WHERE cveArticulo = :cveArticulo');
-        $select->bindValue('cveArticulo', $cveArticulo);
-        $select->execute();
+	public function obtenerArticulo($cveArticulo) {
+		$db = Db::conectar();
+		$select = $db->prepare('SELECT * FROM Articulos WHERE cveArticulo = :cveArticulo');
+		$select->bindValue('cveArticulo', $cveArticulo);
+		$select->execute();
 
-        $articulo = $select->fetch();
-        $myArticulo = new Articulo();
-        $myArticulo->setCveArticulo($articulo['cveArticulo']);
-        $myArticulo->setNombre($articulo['nombre']);
-        $myArticulo->setPrecio($articulo['precio']);
-
-        return $myArticulo;
-    }
+		$articulo = $select->fetch();
+		$myArticulo = new Articulos();
+		$myArticulo->setCveArticulo($articulo['cveArticulo']);
+		$myArticulo->setNombre($articulo['nombre']);
+		$myArticulo->setPrecio($articulo['precio']);
+		return $myArticulo;
+	}
 
 	// Funcion Actualizar
-    public function actualizar($articulo) {
-        $db = Db::conectar();
-        $actualizar = $db->prepare('UPDATE Articulos SET nombre = :nombre, precio = :precio WHERE cveArticulo = :cveArticulo');
-        $actualizar->bindValue('cveArticulo', $articulo->getCveArticulo());
-        $actualizar->bindValue('nombre', $articulo->getNombre());
-        $actualizar->bindValue('precio', $articulo->getPrecio());
-        $actualizar->execute();
-    }
+	public function actualizar($articulo) {
+		$db = Db::conectar();
+		$actualizar = $db->prepare('UPDATE Articulos SET nombre = :nombre, precio = :precio WHERE cveArticulo = :cveArticulo');
+		$actualizar->bindValue('cveArticulo', $articulo->getCveArticulo());
+		$actualizar->bindValue('nombre', $articulo->getNombre());
+		$actualizar->bindValue('precio', $articulo->getPrecio());
+		$actualizar->execute();
+	}
 }
 ?>
--- ventas2/mostrar-admin.php
+++ ventas2/mostrar-admin.php
@@ -1,14 +1,14 @@
- <!-- ///////////////////////////////////////////// -->
- <!-- ///////////////////////////////////////////// -->
+<!-- Inicia Conexion con la base de datos -->
 <?php
-//incluye la clase crud y articulo
-require_once('crud.php');
-require_once('articulo.php');
-$crud=new crudArticulo();
-$articulo= new Articulos();
-//obtiene todos los articulos con el método mostrar de la clase crud
-$listaArticulos=$crud->mostrar();
-?>
+ //incluye la clase crud y articulo
+ require_once('crud.php');
+ require_once('articulo.php');
+ $crud=new crudArticulo();
+ $articulo= new Articulos();
+ //obtiene todos los articulos con el método mostrar de la clase crud
+ $listaArticulos=$crud->mostrar();
+ ?>
+ <!-- Termina Conexion con la base de datos -->
 
 <!DOCTYPE html>
 <html lang="en">
@@ -17,133 +17,112 @@ $listaArticulos=$crud->mostrar();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cafeteria</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style.php">
     <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
-</head>
-<a name="inicio"></a><!-- Vinculos del menu a direccionar Productos -->
-<a name="index.html"></a><!-- Vinculos del menu a direccionar sesion -->
-<a name="sesion.php"></a>
-<a name="registro.php"></a>
-</html>
-
-
-
-<html>
-<head>
-	<title>Mostrar articulos</title>
 </head>
 <body>
 
-<!-- ///////////////////////////////////////////// -->
 <header>
-        <a href="#" class="logo">
-            <img src="img/logo.png" alt="">
-        </a>
-        Eliminar
-        <i class='bx bx-menu' id="menu-icon"></i>
-        <ul class="navbar">
-            <li><a href="#inicio">Inicio</a></li>   
-            <li><a href="#producttos">Productos</a></li> <!-- Vinculos del menu a direccionar -->
-            <li><a href="#direccion">Dirección y Contacto</a></li>
-            <li><a href="sesion.php">Iniciar Sesión</a></li> <!-- iniciar sesion -->
-        </ul>
-
-        <div class="header-icon">
-            <a href="carrito-principal.php"><img src="img/carrito.png" alt="carrito" loading="lazy"></a>
-        </div>
-    </header>
-      
-	<section class="home" id="home">
-        <div class="home-text">
-            <h1>¿Listo para despertar?</h1>
-            <p>Bienvenido seas a nuestra cafeteria, contamos con gran variedad de sabores</p>
-        </div>
-        
-        <div class="home-img"><!-- Imagen de inicio-->
-            <img src="img/main.png" alt="">
-
-        </div>
-    </section>
-    <center>
-
-    <!-- Panel de control de ingreso de productos -->
+    <!-- logo de cafeteria -->
+    <a href="#" class="logo">
+        <img src="img/logo.png" alt="">
+    </a>
     
-    <table border=1 cellpadding="0" cellspacing="5" width="30%" bgcolor="FFFFFF">   
-    <tr>
-        <td><a href="ingresar.php">Ingresar Más Productos</a></td>
-        <td><a href="mostrar.php">Refrescar Productos</a></td>
-    </tr>
+    <!-- Menu -->
+    <ul class="navbar">
+        <li><a href="#inicio">Inicio</a></li>   
+        <li><a href="#producttos">Productos</a></li>
+        <li><a href="#direccion">Dirección y Contacto</a></li>
+        <li><a href="sesion.php">Iniciar Sesión</a></li>
+    </ul>
+
+    <!-- logo carrito -->
+    <div class="header-icon">
+        <a href="carrito-principal.php"><img src="img/carrito.png" alt="carrito" loading="lazy"></a>
+    </div>
+</header>
+  
+<section class="home" id="home">
+    <div class="home-text">
+        <h1>¿Listo para despertar?</h1>
+        <p>Bienvenido seas a nuestra cafetería, contamos con gran variedad de sabores</p>
+    </div>
+    
+    <div class="home-img">
+        <img src="img/main.png" alt="">
+    </div>
+</section>
+
+<center>
+    <table border="1" cellpadding="0" cellspacing="5" width="30%" bgcolor="FFFFFF">
+        <tr>
+            <td><a href="ingresar.php">Ingresar Más Productos</a></td>
+            <td><a href="mostrar.php">Refrescar Productos</a></td>
+        </tr>
     </table>
-        <!-- ///////////////////////////////////////////////////// -->
 
-	<!-- ///////////////////////////////////////////// -->
-    
- 	<a name="producttos"></a> <!-- Vinculos del menu a direccionar Productos -->
-    	<section class="products" id="products">
-            <div class="heading">        
-                <h2>Productos</h2>
-            </div>
+    <section class="products" id="products">
+        <div class="heading">        
+            <h2>Productos</h2>
+        </div>
 
-            <!-- FOR que imprime todos los productos -->
-            <?php foreach ($listaArticulos as $articulo) {?>
-			<div class="products-container">
-                
+        <?php foreach ($listaArticulos as $articulo) { ?>
+            <div class="products-container">
                 <div class="box">
                     <img src="img/frape.png" alt="">
-                    <h3><?php echo $articulo->getNombre() ?></h3><!--Muestra el nombre del producto -->
+                    <h3><?php echo $articulo->getNombre() ?></h3>
                     <div class="content">
-                        <span> $<?php echo $articulo->getPrecio() ?> Pesos </span><!--Muestra el precio del producto -->
-                        <a href="actualizar.php?cveArticulo=<?php echo $articulo->getCveArticulo()?>&accion=a">Actualizar</a>
-                        <a href="admin_articulo.php?cveArticulo=<?php echo $articulo->getCveArticulo()?>&accion=e">Eliminar</a>
+                        <span>$<?php echo $articulo->getPrecio() ?> Pesos</span>
+                        <a href="actualizar.php?cveArticulo=<?php echo $articulo->getCveArticulo() ?>&accion=a">Actualizar</a>
+                        <a href="admin_articulo.php?cveArticulo=<?php echo $articulo->getCveArticulo() ?>&accion=e">Eliminar</a>
                     </div>
                 </div>
             </div>
-            <?php }?>
+        <?php } ?>
 
-            <br>
-                <table border=1>
-                    <head>
-                        <td>cveArticulos</td>
-                        <td>nombre</td>
-			            <td>precio</td>
-                    </head>
-                    <body>
-                        <?php foreach ($listaArticulos as $articulo) {?>
-                            <tr>
-                                <td><?php echo $articulo->getCveArticulo() ?></td>
-                                <td><?php echo $articulo->getNombre() ?></td>
-                                <td><?php echo $articulo->getPrecio() ?></td>
-			
-                                <td><a href="actualizar.php?cveArticulo=<?php echo $articulo->getCveArticulo()?>&accion=a">Actualizar</a> </td>
-				                <td><a href="admin_articulo.php?cveArticulo=<?php echo $articulo->getCveArticulo()?>&accion=e">Eliminar</a> </td>
-                            </tr>
-                            <?php }?>
-                        </body>
-                    </table>
-
-                    
-                <br>
-                <!-- Tabla para ingresar mas productos -->
-                <table border=1 cellspacing="5" width="30%" bgcolor="FFFFFF">   
+        <br>
+        <table border="1">
+            <thead>
+                <tr>
+                    <th>cveArticulos</th>
+                    <th>nombre</th>
+                    <th>precio</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($listaArticulos as $articulo) { ?>
                     <tr>
-                        <td><a href="ingresar.php">Ingresar Más Productos</a></td>
-                        <td><a href="mostrar.php">Refrescar Productos</a></td>
+                        <td><?php echo $articulo->getCveArticulo() ?></td>
+                        <td><?php echo $articulo->getNombre() ?></td>
+                        <td><?php echo $articulo->getPrecio() ?></td>
+                        <td><a href="actualizar.php?cveArticulo=<?php echo $articulo->getCveArticulo() ?>&accion=a">Actualizar</a></td>
+                        <td><a href="admin_articulo.php?cveArticulo=<?php echo $articulo->getCveArticulo() ?>&accion=e">Eliminar</a></td>
                     </tr>
-                </table>
-                </center>
-            </section>
-            <a name="direccion"></a> <!-- Vinculos del menu a direccionar Productos -->
-            <div class="footer">
-                <p><h3> Contacto: 9831234567 <br> Dirección:  Av. Andres Quintana Roo entre Insurgentes y Juan José Siorda! https://goo.gl/maps/ik7cVLSPsWrWVMNT9</h3></p>
-            </div>
-            <script src="https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1"></script>
-            <df-messenger
-                chat-title="Cafeteria_IA"
-                agent-id="39350e75-1a68-444d-bc1d-66843bd6dca3"
-                language-code="es"
-            ></df-messenger>
-        </body>
-        </html>
- <!-- ///////////////////////////////////////////// -->
-  <!-- ///////////////////////////////////////////// -->
+                <?php } ?>
+            </tbody>
+        </table>
 
+        <br>
+        <table border="1" cellspacing="5" width="30%" bgcolor="FFFFFF">
+            <tr>
+                <td><a href="ingresar.php">Ingresar Más Productos</a></td>
+                <td><a href="mostrar.php">Refrescar Productos</a></td>
+            </tr>
+        </table>
+    </section>
+
+    <a name="direccion"></a>
+    <div class="footer">
+        <p><h3>Contacto: 9831234567 <br> Dirección: Av. Andres Quintana Roo entre Insurgentes y Juan José Siorda! https://goo.gl/maps/ik7cVLSPsWrWVMNT9</h3></p>
+    </div>
+
+    <script src="https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1"></script>
+    <df-messenger
+        chat-title="Cafeteria_IA"
+        agent-id="39350e75-1a68-444d-bc1d-66843bd6dca3"
+        language-code="es"
+    ></df-messenger>
+</center>
+
+</body>
+</html>
